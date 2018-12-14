c998722cfe52267c30986bedd31be9ac640cc188
Suneet-M <43340251+Suneet-M@users.noreply.github.com>
2018-12-14 22:18:01 +0530

    Initial Commit - init angular project

---
+++ app/scripts/app.js
@@ -0,0 +1,12 @@
+'use strict';
+
+/**
+ * @ngdoc overview
+ * @name yummyStationApp
+ * @description
+ * # yummyStationApp
+ *
+ * Main module of the application.
+ */
+angular
+  .module('yummyStationApp', []);
---
+++ app/scripts/controllers/main.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name yummyStationApp.controller:MainCtrl
+ * @description
+ * # MainCtrl
+ * Controller of the yummyStationApp
+ */
+angular.module('yummyStationApp')
+  .controller('MainCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
