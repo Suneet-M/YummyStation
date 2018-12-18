ebaca13bf5f1d5945efac70f68d4ba40357155e4
Suneet-M <43340251+Suneet-M@users.noreply.github.com>
2018-12-18 23:18:22 +0530

    feat: Add handler for ordering same dish twice

--- app/scripts/services/cartmanager.js
+++ app/scripts/services/cartmanager.js
@@ -12,11 +12,28 @@ angular.module('yummyStationApp')
 		this.orderList = {};
 
 		this.addToCart = function(foodType, foodItem) {
-			if(!this.orderList[foodType]) {
-				this.orderList[foodType] = [];
+			let count, //For number of orders of same dish
+			//Assign to a variable for ease of typing
+			foodTypeInOrderList = this.orderList[foodType];
+
+			//Create a key for the food type if it doesn't exist already
+			if(!foodTypeInOrderList) {
+				foodTypeInOrderList = {};
 			}
-			this.orderList[foodType].push(foodItem);
+
+			//Checks if the food item is already added to orders, then increases the count
+			if(foodTypeInOrderList[foodItem]) {
+				count = foodTypeInOrderList[foodItem];
+				count++;
+			} else {
+				count = 1;
+			}
+
+			foodTypeInOrderList[foodItem] = count;
 			console.log('New order added');
+
+			//Passing the update back to orderList
+			this.orderList[foodType] = foodTypeInOrderList;
 		};
 
 		this.getCart = function() {
